e0e18c5200c77575146d7982db99112893ef2cd7
Daniel Portugal <daniel.epr77@gmail.com>
2021-09-07 02:49:46 +0000

    ejercicio 01

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3
+"""
+    Author:  Danieledu <daniel.epr77@gmail.com>
+    Purpose: Say hello
+"""
 
-# Purpose: Say hello
-print('Hello, World!')
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument(
+        '-n', '--name', metavar='name',
+        default='World',
+        help='Name to greet'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
